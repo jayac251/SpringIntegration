68d49c526b23091d25637e192893f7d85266ea8e
Chitra <Chitra@DESKTOP-1BHEOKR>
2018-08-08 09:01:17 -0700

    Inital project

--- demo/src/main/java/com/springintegration/demo/DemoApplication.java
+++ demo/src/main/java/com/springintegration/demo/DemoApplication.java
@@ -1,12 +1,24 @@
 package com.springintegration.demo;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class DemoApplication {
+@Configuration
+@ImportResource("integration-context.xml")
+public class DemoApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 	}
 }
